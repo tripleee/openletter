4efa1877af2be7b847b7a15bf7b7eec367dfb08c
Oleg Valter <o.a.philippov@gmail.com>
2023-06-02 15:41:49 +0300

    fix linting issues; add NPM script for autofixing linter issues

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,36 @@
 $(() => {
-    let expanded = false;
-    let text;
+  let expanded = false;
+  let text;
 
-    $('.js-expand-signatories').on('click', evt => {
-        evt.preventDefault();
-        if (expanded) {
-            $('.signatory-panel, .shadow').css({
-                'max-height': '30em',
-                'overflow-y': 'scroll'
-            });
-            $(evt.target).html(text);
-        }
-        else {
-            text = $(evt.target).html();
-            $('.signatory-panel, .shadow').css({
-                'max-height': 'unset',
-                'overflow-y': 'hidden'
-            });
-            $(evt.target).html('Contract &uarr;');
-        }
-        expanded = !expanded;
-    });
+  $('.js-expand-signatories').on('click', evt => {
+    evt.preventDefault();
+    if (expanded) {
+      $('.signatory-panel, .shadow').css({
+        'max-height': '30em',
+        'overflow-y': 'scroll'
+      });
+      $(evt.target).html(text);
+    }
+    else {
+      text = $(evt.target).html();
+      $('.signatory-panel, .shadow').css({
+        'max-height': 'unset',
+        'overflow-y': 'hidden'
+      });
+      $(evt.target).html('Contract &uarr;');
+    }
+    expanded = !expanded;
+  });
 
-    let darkMode = false;
+  let darkMode = false;
 
-    $('.color-mode-toggle').on('click', evt => {
-        if (darkMode) {
-            $('#dark-sheet').attr('href', '#');
-        }
-        else {
-            $('#dark-sheet').attr('href', '/stylesheets/dark.css');
-        }
-        darkMode = !darkMode;
-    });
+  $('.color-mode-toggle').on('click', () => {
+    if (darkMode) {
+      $('#dark-sheet').attr('href', '#');
+    }
+    else {
+      $('#dark-sheet').attr('href', '/stylesheets/dark.css');
+    }
+    darkMode = !darkMode;
+  });
 });
