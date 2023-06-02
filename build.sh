065c9dfdd884f4bb7ea70edcb1b1baf064cc1f24
Oleg Valter <o.a.philippov@gmail.com>
2023-06-02 18:03:10 +0300

    build scripts should  create build/public/javascripts, or uglifyjs will fail if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 echo "> tsc -t es5 --lib es7 --outDir build ./*.ts" &&
 tsc -t es5 --lib es7 --outDir build ./*.ts &&
+echo "> mkdir -p build/public/javascripts" &&
+mkdir -p build/public/javascripts &&
 echo "> uglifyjs public/javascripts/*.js -c --source-map -o build/public/javascripts/application.js" &&
 uglifyjs public/javascripts/*.js -c --source-map -o build/public/javascripts/application.js &&
 echo "> cp -r public/stylesheets build/public" &&
